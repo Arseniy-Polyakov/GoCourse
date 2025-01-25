073d1189e057f4546c3684997f3e60f46df2884b
Arseniy-Polyakov <arseney02@mail.ru>
2025-01-25 22:03:47 +0300

    The Second Increment. Unit Tests

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"math/rand"
 	"net/http"
 	"strings"
@@ -14,54 +12,44 @@ var (
 	urlYandex = "https://practicum.yandex.ru"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Не удалось прочитать тело запроса", http.StatusBadRequest)
-			return
-		}
-		url := string(body)
+func HandlerPost(w http.ResponseWriter, r *http.Request) {
+	// body, err := io.ReadAll(r.Body)
+	// if err != nil {
+	// 	http.Error(w, "Не удалось прочитать тело запроса", http.StatusBadRequest)
+	// 	return
+	// }
+	// url := string(body)
 
-		symbols := "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
-		shortPart := []string{}
+	symbols := "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
+	shortPart := []string{}
 
-		rand.Seed(time.Now().UnixNano())
-		for i := 0; i < 8; i++ {
-			randomNumber := rand.Intn(len(symbols))
-			randomSymbol := string(symbols[randomNumber])
-			shortPart = append(shortPart, randomSymbol)
-		}
-
-		shortPartStr := strings.Join(shortPart, "")
-		shortLink := "http://localhost:8080/" + shortPartStr
-
-		links[shortPartStr] = urlYandex
-
-		w.WriteHeader(http.StatusCreated)
-		w.Header().Set("Content-Type", "text/plain")
-		w.Write([]byte(shortLink))
-		fmt.Println(url)
-		fmt.Println(http.StatusCreated)
-		fmt.Println(shortLink)
-		fmt.Println(links)
-	})
-
-	http.HandleFunc("/{shortlink}", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			http.Error(w, "The method GET is not allowed", http.StatusMethodNotAllowed)
-			return
-		}
-		shortLink := r.URL.Path[len("/"):]
-		fullLink := links[shortLink]
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(fullLink))
-		fmt.Println(fullLink)
-	})
-
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Print("Server Error")
-	} else {
-		fmt.Println("OK")
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < 8; i++ {
+		randomNumber := rand.Intn(len(symbols))
+		randomSymbol := string(symbols[randomNumber])
+		shortPart = append(shortPart, randomSymbol)
 	}
+
+	shortPartStr := strings.Join(shortPart, "")
+	shortLink := "http://localhost:8080/" + shortPartStr
+
+	links[shortPartStr] = urlYandex
+
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte(shortLink))
+}
+
+func HandlerGet(w http.ResponseWriter, r *http.Request) {
+	shortLink := r.URL.Path[len("/"):]
+	fullLink := links[shortLink]
+	// w.WriteHeader(http.StatusCreated)
+	// w.Write([]byte(fullLink))
+	http.Redirect(w, r, fullLink, http.StatusTemporaryRedirect)
+}
+
+func main() {
+	http.HandleFunc("/", HandlerPost)
+	http.HandleFunc("/{shortLink}", HandlerGet)
+	http.ListenAndServe(":8080", nil)
 }
